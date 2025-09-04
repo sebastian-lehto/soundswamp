5fe54c0e53b210b73d353e06345ebf7eb3a89f7a
Sebastian <sebastian.lehto@gmail.com>
2025-09-04 16:22:22 +0300

    cypress config fix

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://server:5000',
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
+    supportFile: 'cypress/support/e2e.{js,ts}'
   },
 });
