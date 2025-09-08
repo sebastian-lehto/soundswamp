d4440e217d010872edd6bcb9ffb5c2db388c7a64
Sebastian <sebastian.lehto@gmail.com>
2025-09-08 15:37:44 +0300

    cypress.config.ts fix

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from "cypress";
-
-export default defineConfig({
+export default {
   e2e: {
     baseUrl: 'http://server:5000',
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
     supportFile: 'cypress/support/e2e.{js,ts}'
   },
-});
+};
